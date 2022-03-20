3df60740595f223a4c7864c6be8500cc136c07a7
shtyrman911 <101965252+shtyrman911@users.noreply.github.com>
2022-03-20 11:37:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,28 @@ this.rate = rate;
 this.used = used;
 this.father = father;
 this.code = code;
+}
+
 
 function FindCode(block) {
 
-if (tree[block.father].code != '"") {
+if (tree[block.father].code != '') {
 block.code = tree[block.father].code + "1°;
 
-bs
+}
 
-(a -we
+else {
 if (block.letter == tree[minIndex].letter) {
 block.code = ‘0°;
 
+}
 else if (block.letter == tree[preminIndex].letter) {
 block.code = ‘1°;
+}
 
-rae
+  esle {
 FindCode(tree[block.father]);
-block.code = tree[block.father].code + '@';
+block.code = tree[block.father].code + '0';
+  }
+}
+}
